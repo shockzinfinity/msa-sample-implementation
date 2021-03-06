cbe49f865c0dfe7d70d70204a5ea8fa07f4f3287
Jun Yu <shockz@me.com>
2021-03-06 18:15:47 +0900

    implement get list endpoint

--- src/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ src/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Catalog.API.Entities;
+using Catalog.API.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog.API.Controllers
 {
+  [Route("api/v1/[controller]")]
+  [ApiController]
   public class CatalogController : ControllerBase
   {
-    public CatalogController()
+    private readonly IProductRepository _repository;
+
+    public CatalogController(IProductRepository repository)
     {
+      _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    {
+      var products = await _repository.GetProducts();
+      return Ok(products);
     }
   }
 }
