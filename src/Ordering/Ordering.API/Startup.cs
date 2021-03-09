8175448343697fbbfa90d1800bf603a75526d201
shockz-pulseHome <shockzinfinity@gmail.com>
2021-03-10 00:23:12 +0900

    complete ordering cqrs implementation

--- src/Ordering/Ordering.API/Startup.cs
+++ src/Ordering/Ordering.API/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -10,7 +12,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Ordering.Application.Handlers;
+using Ordering.Core.Repositories;
+using Ordering.Core.Repositories.Base;
 using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repositories;
+using Ordering.Infrastructure.Repositories.Base;
 
 namespace Ordering.API
 {
@@ -29,6 +37,19 @@ namespace Ordering.API
       services.AddControllers();
 
       services.AddDbContext<OrderContext>(c => c.UseSqlServer(Configuration.GetConnectionString("OrderConnection")), ServiceLifetime.Singleton);
+
+      services.AddAutoMapper(typeof(Startup));
+
+      services.AddMediatR(typeof(CheckoutOrderHandler).GetTypeInfo().Assembly);
+
+      services.AddTransient<IOrderRepository, OrderRepository>(); // for query
+      services.AddScoped(typeof(IRepository<>), typeof(Repository<>)); // for mediatR
+      services.AddScoped(typeof(IOrderRepository), typeof(OrderRepository)); // for mediatR -> TODO: 파악 필요, 왜 scoped 인가?
+
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Order API", Version = "v1" });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +67,12 @@ namespace Ordering.API
       app.UseEndpoints(endpoints =>
       {
         endpoints.MapControllers();
+      });
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Order API V1");
       });
     }
   }
